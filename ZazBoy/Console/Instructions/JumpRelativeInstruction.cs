a123097c50dbffb0836158f7cdb91afbd0f187b0
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-03-19 02:02:30 +0000

    Add Relative Jump Instruction

---
+++ ZazBoy/Console/Instructions/JumpRelativeInstruction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZazBoy.Console.Instructions
+{
+    public class JumpRelativeInstruction : Instruction
+    {
+        public JumpRelativeInstruction(byte opcode) : base(0x00, opcode, 8)
+        {
+            CPU cpu = GameBoy.Instance().CPU;
+            if ((opcode == 0x18) || (opcode == 0x20 && !cpu.zeroFlag) || (opcode == 0x30 && !cpu.carryFlag) || (opcode == 0x28 && cpu.zeroFlag) || (opcode == 0x38 && cpu.carryFlag))
+                totalClocks = 12;
+        }
+
+        protected override void Execute()
+        {
+            CPU cpu = GameBoy.Instance().CPU;
+            switch (opcode)
+            {
+                case 0x18:
+                    ApplyRelativeJump(cpu, true);
+                    break;
+                case 0x20:
+                    ApplyRelativeJump(cpu, !cpu.zeroFlag);
+                    break;
+                case 0xC8:
+                    ApplyRelativeJump(cpu, cpu.zeroFlag);
+                    break;
+                case 0x30:
+                    ApplyRelativeJump(cpu, !cpu.carryFlag);
+                    break;
+                case 0x38:
+                    ApplyRelativeJump(cpu, cpu.carryFlag);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Enacts the effect of the relative jump, setting PC if condition is true, or incrementing it if condition is false
+        /// </summary>
+        /// <param name="cpu">Context</param>
+        /// <param name="condition">The flag to check</param>
+        private void ApplyRelativeJump(CPU cpu, bool condition)
+        {
+            sbyte signedByte = unchecked((sbyte)Get8BitImmediate()); //Even if unused, this ensures PC is incremented
+            if (condition)
+            {
+                ushort address = (ushort)(0xFF00 + signedByte);
+                cpu.programCounter = address;
+            }
+        }
+    }
+}
