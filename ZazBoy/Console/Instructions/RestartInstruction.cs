04779deb1dcee99c59bf82315a74d6dda514b29f
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-03-19 02:03:46 +0000

    Add Restart Instruction

---
+++ ZazBoy/Console/Instructions/RestartInstruction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZazBoy.Console.Instructions
+{
+    public class RestartInstruction : Instruction
+    {
+        public RestartInstruction(byte opcode) : base(0x00, opcode, 16)
+        {
+
+        }
+
+        protected override void Execute()
+        {
+            CPU cpu = GameBoy.Instance().CPU;
+            cpu.PushToStack(cpu.programCounter);
+            switch (opcode)
+            {
+                case 0xC7:
+                    cpu.programCounter = 0x00;
+                    break;
+                case 0xCF:
+                    cpu.programCounter = 0x08;
+                    break;
+                case 0xD7:
+                    cpu.programCounter = 0x10;
+                    break;
+                case 0xDF:
+                    cpu.programCounter = 0x18;
+                    break;
+                case 0xE7:
+                    cpu.programCounter = 0x20;
+                    break;
+                case 0xEF:
+                    cpu.programCounter = 0x28;
+                    break;
+                case 0xF7:
+                    cpu.programCounter = 0x30;
+                    break;
+                case 0xFF:
+                    cpu.programCounter = 0x38;
+                    break;
+            }
+        }
+    }
+}
