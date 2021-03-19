e7b132d5471cea0ead2078ea7d5dfe34b44bd63c
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-03-19 02:03:30 +0000

    Add RET and framework for RETI

---
+++ ZazBoy/Console/Instructions/ReturnEnableInterruptsInstruction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZazBoy.Console.Instructions
+{
+    public class ReturnEnableInterruptsInstruction : Instruction
+    {
+        public ReturnEnableInterruptsInstruction() : base(0x00, 0xD9, 16)
+        {
+        }
+
+        protected override void Execute()
+        {
+            CPU cpu = GameBoy.Instance().CPU;
+            ushort address = cpu.PopFromStack();
+            cpu.programCounter = address;
+            //TODO: Enable Interrupts Master Enable
+        }
+    }
+}
---
+++ ZazBoy/Console/Instructions/ReturnInstruction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZazBoy.Console.Instructions
+{
+    public class ReturnInstruction : Instruction
+    {
+        public ReturnInstruction(byte opcode) : base(0x00, opcode, 8)
+        {
+            CPU cpu = GameBoy.Instance().CPU;
+            if ((opcode == 0xC0 && !cpu.zeroFlag) || (opcode == 0xD0 && !cpu.carryFlag) || (opcode == 0xC8 && cpu.zeroFlag) || (opcode == 0xD8 && cpu.carryFlag))
+                totalClocks = 20;
+            else if (opcode == 0xC9)
+                totalClocks = 16;
+        }
+
+        protected override void Execute()
+        {
+            CPU cpu = GameBoy.Instance().CPU;
+            switch (opcode)
+            {
+                case 0xC0:
+                    ApplyReturn(cpu, !cpu.zeroFlag);
+                    break;
+                case 0xC9:
+                    ApplyReturn(cpu, true);
+                    break;
+                case 0xC8:
+                    ApplyReturn(cpu, cpu.zeroFlag);
+                    break;
+                case 0xD0:
+                    ApplyReturn(cpu, !cpu.carryFlag);
+                    break;
+                case 0xD8:
+                    ApplyReturn(cpu, cpu.carryFlag);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Enacts the effect of the return, setting PC & SP if condition is true
+        /// </summary>
+        /// <param name="cpu">Context</param>
+        /// <param name="condition">The flag to check</param>
+        private void ApplyReturn(CPU cpu, bool condition)
+        {
+            if (condition)
+            {
+                ushort address = cpu.PopFromStack();
+                cpu.programCounter = address;
+            }
+        }
+    }
+}
