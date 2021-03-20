8c734c673bc0bfb9c3c559cd4e69a2f432649fc0
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-03-20 00:05:45 +0000

    Add Operation as a base to Instruction

--- ZazBoy/Console/Instructions/Instruction.cs
+++ ZazBoy/Console/Instructions/Instruction.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZazBoy.Console.Operations;
 
 namespace ZazBoy.Console.Instructions
 {
     /// <summary>
     /// A "Command" class responsible for the execution of a CPU instruction
     /// </summary>
-    public abstract class Instruction
+    public abstract class Instruction : Operation
     {
         /// <summary>
         /// The opcode for the instruction.
@@ -19,19 +20,6 @@ namespace ZazBoy.Console.Instructions
         /// The prefix to signify use of a different optable (only used for bit operations - 0xCB)
         /// </summary>
         public byte opcodePrefix { get; private set; }
-        /// <summary>
-        /// The total number of clocks required to execute this instruction.<br></br>
-        /// Some instructions, such as conditional jumps, may modify the required clocks in the constructor depending on the outcome.
-        /// </summary>
-        public int totalClocks { get; protected set; }
-        /// <summary>
-        /// The number of clocks executed against this instruction.
-        /// </summary>
-        public int executedClocks { get; private set; }
-        /// <summary>
-        /// Signifies if this instruction has completed execution
-        /// </summary>
-        public bool isComplete { get; protected set; }
 
         /// <summary>
         /// Creates a new instruction command
@@ -39,52 +27,20 @@ namespace ZazBoy.Console.Instructions
         /// <param name="opcodePrefix">The prefix to signify use of a different optable (only used for bit operations - 0xCB)</param>
         /// <param name="opcode">The opcode for the instruction</param>
         /// <param name="clocks">The number of clocks required for the instruction. (This may change during execution due to conditional statements)</param>
-        public Instruction(byte opcodePrefix, byte opcode, int clocks)
+        public Instruction(byte opcodePrefix, byte opcode, int clocks) : base(clocks)
         {
             this.opcode = opcodePrefix;
             this.opcode = opcode;
-            this.totalClocks = clocks;
-            this.executedClocks = 0;
         }
 
         /// <summary>
         /// Progresses the instruction by one clock.
         /// </summary>
-        public virtual void Tick()
+        public override void Tick()
         {
             if (executedClocks == 0)
-            {
-                System.Console.WriteLine(GameBoy.Instance().CPU.programCounter+": Running "+this.GetType().Name+" (" + opcode + ")");
-                Execute();
-            }
-            executedClocks++;
-            if (executedClocks == totalClocks)
-                isComplete = true;
-        }
-
-        protected abstract void Execute();
-
-        protected byte Get8BitImmediate()
-        {
-            MemoryMap memMap = GameBoy.Instance().MemoryMap;
-            CPU cpu = GameBoy.Instance().CPU;
-            byte value = memMap.Read(cpu.programCounter);
-            cpu.programCounter++;
-            return value;
-        }
-
-        protected ushort Get16BitImmediate()
-        {
-            MemoryMap memMap = GameBoy.Instance().MemoryMap;
-            CPU cpu = GameBoy.Instance().CPU;
-            byte lsb = memMap.Read(cpu.programCounter);
-            cpu.programCounter++;                       //Game Boy is little endian, so LSB then MSB
-            byte msb = memMap.Read(cpu.programCounter);
-            cpu.programCounter++;
-
-            ushort value = (ushort)(msb * 0x100);
-            value += lsb;
-            return value;
+                System.Console.WriteLine("Opcode: " + opcodePrefix +" | "+ opcode);
+            base.Tick();
         }
     }
 }
---
+++ ZazBoy/Console/Operations/Operation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZazBoy.Console.Operations
+{
+    /// <summary>
+    /// A "Command" class responsible for the execution of a CPU operation
+    /// </summary>
+    public abstract class Operation
+    {
+        /// <summary>
+        /// The total number of clocks required to execute this operation.
+        /// </summary>
+        public int totalClocks { get; protected set; }
+        /// <summary>
+        /// The number of clocks executed against this operation.
+        /// </summary>
+        public int executedClocks { get; private set; }
+        /// <summary>
+        /// Signifies if this operation has completed execution
+        /// </summary>
+        public bool isComplete { get; protected set; }
+
+        /// <summary>
+        /// Creates a new operation command
+        /// </summary>
+        /// <param name="clocks">The number of clocks required for the operation. (This may change during execution)</param>
+        public Operation(int clocks)
+        {
+            this.totalClocks = clocks;
+            this.executedClocks = 0;
+        }
+
+        /// <summary>
+        /// Progresses the operation by one clock.
+        /// </summary>
+        public virtual void Tick()
+        {
+            if (executedClocks == 0)
+            {
+                System.Console.WriteLine(GameBoy.Instance().CPU.programCounter + ": Running " + this.GetType().Name);
+                Execute();
+            }
+            executedClocks++;
+            if (executedClocks == totalClocks)
+                isComplete = true;
+        }
+
+        protected abstract void Execute();
+
+        protected byte Get8BitImmediate()
+        {
+            MemoryMap memMap = GameBoy.Instance().MemoryMap;
+            CPU cpu = GameBoy.Instance().CPU;
+            byte value = memMap.Read(cpu.programCounter);
+            cpu.programCounter++;
+            return value;
+        }
+
+        protected ushort Get16BitImmediate()
+        {
+            MemoryMap memMap = GameBoy.Instance().MemoryMap;
+            CPU cpu = GameBoy.Instance().CPU;
+            byte lsb = memMap.Read(cpu.programCounter);
+            cpu.programCounter++;                       //Game Boy is little endian, so LSB then MSB
+            byte msb = memMap.Read(cpu.programCounter);
+            cpu.programCounter++;
+
+            ushort value = (ushort)(msb * 0x100);
+            value += lsb;
+            return value;
+        }
+    }
+}
