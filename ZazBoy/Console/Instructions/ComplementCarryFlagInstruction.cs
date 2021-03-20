abccc88ab8011cf85ba33c0fe329c5bbadcb3b11
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-03-20 00:06:09 +0000

    Add Carry Flag instructions

---
+++ ZazBoy/Console/Instructions/ComplementCarryFlagInstruction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZazBoy.Console.Instructions
+{
+    class ComplementCarryFlagInstruction : Instruction
+    {
+        public ComplementCarryFlagInstruction() : base(0x00, 0x3F, 4)
+        {
+
+        }
+
+        protected override void Execute()
+        {
+            CPU cpu = GameBoy.Instance().CPU;
+            cpu.subtractionFlag = false;
+            cpu.halfCarryFlag = false;
+            cpu.carryFlag = !cpu.carryFlag;
+        }
+    }
+}
---
+++ ZazBoy/Console/Instructions/SetCarryFlagInstruction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZazBoy.Console.Instructions
+{
+    class SetCarryFlagInstruction : Instruction
+    {
+        public SetCarryFlagInstruction() : base(0x00, 0x37, 4)
+        {
+
+        }
+
+        protected override void Execute()
+        {
+            CPU cpu = GameBoy.Instance().CPU;
+            cpu.subtractionFlag = false;
+            cpu.halfCarryFlag = false;
+            cpu.carryFlag = true;
+        }
+    }
+}
