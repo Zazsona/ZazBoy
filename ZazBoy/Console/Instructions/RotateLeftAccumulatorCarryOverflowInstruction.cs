ff65d8bfb80f2a86c4dfd8a68f16748333e5dcb9
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-03-21 18:25:30 +0000

    Add Rotate Left/Right Akku w/ Carry over/underflow

---
+++ ZazBoy/Console/Instructions/RotateLeftAccumulatorCarryOverflowInstruction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZazBoy.Console.Instructions
+{
+    public class RotateLeftAccumulatorCarryOverflowInstruction : Instruction
+    {
+        public RotateLeftAccumulatorCarryOverflowInstruction(byte opcode) : base(0x00, opcode, 4)
+        {
+
+        }
+
+        protected override void Execute()
+        {
+            CPU cpu = GameBoy.Instance().CPU;
+            byte value = cpu.registerA;
+
+            byte droppedBitMask = (1 << 7);
+            bool droppedBit = ((value & droppedBitMask) != 0);
+            byte shiftedValue = ((byte)(value << 1));
+
+            cpu.registerA = shiftedValue;
+            cpu.carryFlag = droppedBit;
+            cpu.zeroFlag = false;
+            cpu.halfCarryFlag = false;
+            cpu.subtractionFlag = false;
+        }
+    }
+}
