b1d32fc3e3e67bc2f9e21e3dbf268984f5c1cc6a
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-03-27 19:39:14 +0000

    Add primitive LCD implementation

--- ZazBoy/Console/LCD.cs
+++ ZazBoy/Console/LCD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,17 +16,44 @@ namespace ZazBoy.Console
         public const int ScreenPixelHeight = 144;
         private bool powered;
 
+        private Bitmap bitmap;
+        private Graphics gfx; //TODO: System.Drawing crap is all temp.
+
         public LCD()
         {
             this.powered = true;
+            bitmap = new Bitmap(160, 144);
+            gfx = Graphics.FromImage(bitmap);
+            Pen pen = new Pen(Color.Red);
+            gfx.FillRectangle(pen.Brush, 0, 0, 160, 144);
         }
 
-        public void DrawPixel()
+        public void DrawPixel(Pixel pixel, byte lineX, byte lineY)
         {
             PPU ppu = GameBoy.Instance().PPU;
             if (!ppu.HasPPUDisabledThisFrame) //DMG disables drawing until the next frame if it's been disabled.
             {
-                //TODO: Fill
+                Color color = Color.Red;
+                switch (pixel.paletteColour)
+                {
+                    case 0:
+                        color = Color.White;
+                        break;
+                    case 1:
+                        color = Color.LightGray;
+                        break;
+                    case 2:
+                        color = Color.DarkGray;
+                        break;
+                    case 3:
+                        color = Color.Black;
+                        break;
+                }
+                Pen pen = new Pen(color);
+                gfx.DrawLine(pen, lineX, lineY, lineX+1, lineY);
+                if (DateTime.Now.Second == 30 || DateTime.Now.Second == 1)
+                    bitmap.Save("H:\\frame.png");
+
             }
         }
 
