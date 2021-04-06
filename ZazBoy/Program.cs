01b4ad3abc9156837f0c17ed1da58bbcf9915311
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-04-06 20:49:02 +0100

    Add Avalonia

--- ZazBoy/Program.cs
+++ ZazBoy/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Avalonia;
+using System;
+using System.Threading;
 using ZazBoy.Console;
 
 namespace ZazBoy
@@ -7,8 +9,21 @@ namespace ZazBoy
     {
         static void Main(string[] args)
         {
+            Thread emulatorThread = new Thread(StartEmulator);
+            emulatorThread.Start();
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+
+        private static void StartEmulator()
+        {
             GameBoy gameBoy = GameBoy.Instance();
             gameBoy.SetPowerOn(true);
         }
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
     }
 }
