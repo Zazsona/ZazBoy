c041fe1b4b90867541ec72093062877341ab64c7
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-04-18 00:31:01 +0100

    Add placeholder buttons and displays for Debug menu

--- ZazBoy/UI/Controls/DebugControl.axaml.cs
+++ ZazBoy/UI/Controls/DebugControl.axaml.cs
@@ -6,6 +6,8 @@ namespace ZazBoy.UI.Controls
 {
     public class DebugControl : UserControl
     {
+        private Grid operationsList;
+
         public DebugControl()
         {
             InitializeComponent();
@@ -16,6 +18,10 @@ namespace ZazBoy.UI.Controls
             AvaloniaXamlLoader.Load(this);
             this.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Stretch;
             this.VerticalAlignment = Avalonia.Layout.VerticalAlignment.Stretch;
+            operationsList = this.FindControl<Grid>("OperationsList");
+
+            Image cpuIcon = this.FindControl<Image>("CPUIcon");
+            cpuIcon.Source = UIUtil.ConvertDrawingBitmapToUIBitmap(Properties.Resources.CPUIcon);
         }
     }
 }
