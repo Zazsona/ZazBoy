7b26f50bb4b96f8285ca10f7c272996e9f701c83
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-04-17 23:09:41 +0100

    Add CPUIcon

--- ZazBoy/Properties/Resources.Designer.cs
+++ ZazBoy/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace ZazBoy.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap CPUIcon {
+            get {
+                object obj = ResourceManager.GetObject("CPUIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap DebugText {
             get {
                 object obj = ResourceManager.GetObject("DebugText", resourceCulture);
