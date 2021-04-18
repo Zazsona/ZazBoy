151c36ad209f977f9e2d7e5faf560f8178c5675b
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-04-18 20:20:34 +0100

    Add OperationBlock setters

--- ZazBoy/UI/Controls/OperationBlock.axaml.cs
+++ ZazBoy/UI/Controls/OperationBlock.axaml.cs
@@ -6,6 +6,9 @@ namespace ZazBoy.UI.Controls
 {
     public class OperationBlock : UserControl
     {
+        private TextBlock mnemonicTag;
+        private TextBlock positionTag;
+
         public OperationBlock()
         {
             InitializeComponent();
@@ -14,6 +17,18 @@ namespace ZazBoy.UI.Controls
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            mnemonicTag = this.FindControl<TextBlock>("MnemonicTag");
+            positionTag = this.FindControl<TextBlock>("PositionTag");
+        }
+
+        public void SetMnemonic(string mnemonic)
+        {
+            mnemonicTag.Text = mnemonic;
+        }
+
+        public void SetPosition(string position)
+        {
+            positionTag.Text = position;
         }
     }
 }
