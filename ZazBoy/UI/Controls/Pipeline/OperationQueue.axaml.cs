907946691bf498a4bc3bebdae2a8889395868d65
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-04-24 23:23:39 +0100

    Add OperationQueue to its own control

---
+++ ZazBoy/UI/Controls/Pipeline/OperationQueue.axaml.cs
@@ -0,0 +1,53 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+
+namespace ZazBoy.UI.Controls.Pipeline
+{
+    public class OperationQueue : UserControl
+    {
+        private OperationBlock[] operationBlocks;
+        private OperationBlock selectedOperationBlock;
+
+        public OperationQueue()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            Grid operationsList = this.FindControl<Grid>("OperationsList");
+            operationBlocks = new OperationBlock[operationsList.RowDefinitions.Count];
+            for (int i = 0; i < operationBlocks.Length; i++)
+            {
+                OperationBlock operationBlock = this.FindControl<OperationBlock>("OperationBlock" + i);
+                operationBlocks[i] = operationBlock;
+                operationBlock.PointerReleased += HandleOperationBlockSelected;
+            }
+        }
+
+        private void HandleOperationBlockSelected(object? sender, Avalonia.Input.PointerReleasedEventArgs e)
+        {
+            if (selectedOperationBlock != null)
+                selectedOperationBlock.SetSelected(false);
+
+            OperationBlock operationBlock = (OperationBlock)sender;
+            if (operationBlock == selectedOperationBlock)
+                selectedOperationBlock = null;
+            else
+                selectedOperationBlock = (OperationBlock)sender;
+        }
+
+        public OperationBlock[] GetOperationBlocks()
+        {
+            return operationBlocks;
+        }
+
+        public OperationBlock GetSelectedBlock()
+        {
+            return selectedOperationBlock;
+        }
+    }
+}
