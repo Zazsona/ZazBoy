b8b1f170dd31f64e36272d024a99a6fa4b3ce320
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-04-24 01:45:55 +0100

    Add NavBarItem

--- ZazBoy/UI/Controls/NavBar.axaml.cs
+++ ZazBoy/UI/Controls/NavBar.axaml.cs
@@ -1,11 +1,14 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace ZazBoy.UI.Controls
 {
     public class NavBar : UserControl
     {
+        private NavBarItem selectedNavBarItem;
+
         public NavBar()
         {
             InitializeComponent();
@@ -16,6 +19,45 @@ namespace ZazBoy.UI.Controls
             AvaloniaXamlLoader.Load(this);
             Image hideButton = this.FindControl<Image>("HideButton");
             hideButton.Source = UIUtil.ConvertDrawingBitmapToUIBitmap(Properties.Resources.HideNavBar);
+
+            NavBarItem emulatorTabButton = this.FindControl<NavBarItem>("EmulatorTabButton");
+            NavBarItem pipelineTabButton = this.FindControl<NavBarItem>("PipelineTabButton");
+            NavBarItem breakpointsTabButton = this.FindControl<NavBarItem>("BreakpointsTabButton");
+            NavBarItem inspectorTabButton = this.FindControl<NavBarItem>("InspectorTabButton");
+            emulatorTabButton.SetName("Emulator");
+            pipelineTabButton.SetName("Pipeline");
+            breakpointsTabButton.SetName("Breakpoints");
+            inspectorTabButton.SetName("Inspector");
+            emulatorTabButton.PointerPressed += HandleItemSelected;
+            pipelineTabButton.PointerPressed += HandleItemSelected;
+            breakpointsTabButton.PointerPressed += HandleItemSelected;
+            inspectorTabButton.PointerPressed += HandleItemSelected;
+            SelectNavBarItem(emulatorTabButton);
+        }
+
+        private void HandleItemSelected(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            NavBarItem navBarItem = (NavBarItem)sender;
+            SelectNavBarItem(navBarItem);
+        }
+
+        private void SelectNavBarItem(NavBarItem navBarItem)
+        {
+            if (navBarItem != selectedNavBarItem)
+            {
+                DeselectNavBarItem();
+                navBarItem.Background = new SolidColorBrush(Color.FromRgb(37, 37, 37));
+                selectedNavBarItem = navBarItem;
+            }
+        }
+
+        private void DeselectNavBarItem()
+        {
+            if (selectedNavBarItem != null)
+            {
+                selectedNavBarItem.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+                selectedNavBarItem = null;
+            }
         }
     }
 }
---
+++ ZazBoy/UI/Controls/NavBarItem.axaml.cs
@@ -0,0 +1,42 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace ZazBoy.UI.Controls
+{
+    public class NavBarItem : UserControl
+    {
+        private Panel itemPanel;
+        private TextBlock itemText;
+
+        public NavBarItem()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            this.itemPanel = this.FindControl<Panel>("NavBarItemPanel");
+            this.itemText = this.FindControl<TextBlock>("NavBarItemText");
+        }
+
+        public void SetName(string name)
+        {
+            this.itemText.Text = name;
+        }
+
+        public void SetSelected(bool selected)
+        {
+            if (selected)
+            {
+                this.Background = new SolidColorBrush(Color.FromRgb(37, 37, 37));
+            }
+            else
+            {
+                this.Background = new SolidColorBrush(Color.FromRgb(0,0,0), 0.0f);
+            }
+        }
+    }
+}
