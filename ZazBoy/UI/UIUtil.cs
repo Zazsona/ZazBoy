ad74b2da150f59a7b1185b802652fae5d45a3556
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-04-25 16:32:33 +0100

    Add hex parsing and validation methods to UIUtil

--- ZazBoy/UI/UIUtil.cs
+++ ZazBoy/UI/UIUtil.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,9 @@ namespace ZazBoy.UI
     /// </summary>
     public static class UIUtil
     {
+        public static readonly Avalonia.Media.Color validTextBoxBorderColor = Avalonia.Media.Color.FromRgb(152, 152, 152);
+        public static readonly Avalonia.Media.Color invalidTextBoxBorderColor = Avalonia.Media.Color.FromRgb(255, 0, 0);
+
         private static InstructionDatabase idb;
 
         /// <summary>
@@ -62,6 +66,68 @@ namespace ZazBoy.UI
             string opcodeHex = "0x" + opcode.ToString("X2");
             InstructionEntry instructionEntry = (isPrefixed) ? GetInstructionDatabase().cbprefixed[opcodeHex] : GetInstructionDatabase().unprefixed[opcodeHex];
             return instructionEntry;
+        }
+
+        /// <summary>
+        /// Checks if the string denotes a valid ushort, ignoring formatting terms such as "0x" or "#"
+        /// </summary>
+        /// <param name="ushortString">The string to validate</param>
+        /// <returns>bool on valid value</returns>
+        public static bool IsHexUShortValid(string ushortString)
+        {
+            try
+            {
+                ushortString = ushortString.Replace("0x", "").Replace("#", "");
+                int value = int.Parse(ushortString, System.Globalization.NumberStyles.HexNumber);
+                return (value >= 0 && value <= ushort.MaxValue);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Creates a ushort value from a string, ignoring formatting terms such as "0x" or "#"
+        /// </summary>
+        /// <param name="ushortString">The string to parse.</param>
+        /// <returns>The ushort value</returns>
+        public static ushort ParseHexUShort(string ushortString)
+        {
+            ushortString = ushortString.Replace("0x", "").Replace("#", "");
+            ushort value = ushort.Parse(ushortString, NumberStyles.HexNumber);
+            return value;
+        }
+
+        /// <summary>
+        /// Checks if the string denotes a valid byte, ignoring formatting terms such as "0x" or "#"
+        /// </summary>
+        /// <param name="byteString">The string to validate</param>
+        /// <returns>bool on valid value</returns>
+        public static bool IsHexByteValid(string byteString)
+        {
+            try
+            {
+                byteString = byteString.Replace("0x", "").Replace("#", "");
+                int value = int.Parse(byteString, System.Globalization.NumberStyles.HexNumber);
+                return (value >= 0 && value <= byte.MaxValue);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Creates a byte value from a hexadecimal string, ignoring formatting terms such as "0x" or "#"
+        /// </summary>
+        /// <param name="byteString">The string to parse.</param>
+        /// <returns>The byte value</returns>
+        public static byte ParseHexByte(string byteString)
+        {
+            byteString = byteString.Replace("0x", "").Replace("#", "");
+            byte value = byte.Parse(byteString, NumberStyles.HexNumber);
+            return value;
         }
     }
 }
